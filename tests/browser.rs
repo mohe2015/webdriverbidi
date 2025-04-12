183ee1ff322abd44d9d35c94a2d8ead942b4d168
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2025-04-12 18:29:31 +0200

    Make test pass

--- tests/browser.rs
+++ tests/browser.rs
@@ -13,7 +13,10 @@ use webdriverbidi::remote::session::SubscriptionRequest;
 mod utils;
 
 mod install_extension {
-    use webdriverbidi::remote::web_extension::{ExtensionArchivePath, ExtensionData, InstallParameters};
+    use webdriverbidi::{
+        error::CommandError,
+        remote::web_extension::{ExtensionArchivePath, ExtensionData, InstallParameters},
+    };
 
     use super::*;
 
@@ -21,7 +24,13 @@ mod install_extension {
     async fn test_nonexistent_extension() -> Result<()> {
         let mut bidi_session = utils::session::init().await?;
 
-        bidi_session.web_extension_install(InstallParameters::new(ExtensionData::ExtensionArchivePath(ExtensionArchivePath::new("doesnotexist".to_owned())))).await?;
+        let err = bidi_session
+            .web_extension_install(InstallParameters::new(ExtensionData::ExtensionArchivePath(
+                ExtensionArchivePath::new("doesnotexist".to_owned()),
+            )))
+            .await
+            .unwrap_err();
+        assert!(matches!(err, CommandError::Error(_)));
 
         utils::session::close(&mut bidi_session).await?;
 
